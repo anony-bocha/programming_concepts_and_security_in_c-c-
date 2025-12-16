1f91978224f00bf699812f1a6039635bb3006a5a
yonatan semate <semateyonatan@gmail.com>
2025-12-16 14:23:41 +0300

    command line argument

--- cmd3.c
+++ cmd3.c
@@ -7,12 +7,15 @@ int main(int argc, char *argv[])
         printf("Please provide at least 3 arguments: <name> <address> <age>\n");
         return 1;
     }
-
-    printf("    Program name: %s\n", argv[0]);
-    printf("    Name: %s\n", argv[1]);
-    printf("    Address: %s\n", argv[2]);
-    printf("Age: %s\n", argv[3]);
-
+    printf("|-----------------------------------|\n");
+    printf("    Program name:>%s\n", argv[0]);
+	printf("|-----------------------------------|\n");
+    printf("    Name:>%s\n", argv[1]);
+	printf("|-----------------------------------|\n");
+    printf("    Address:>%s\n", argv[2]);
+	printf("|-----------------------------------|\n");
+    printf("    Age:> %s\n", argv[3]);
+    printf("|-----------------------------------|\n");
     return 0;
 }
 
