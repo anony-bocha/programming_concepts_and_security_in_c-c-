aa57c32ab965be86bdcaec7cebd7b760800a17ac
anony <semateyonatan@gmail.com>
2025-12-23 14:46:30 +0300

    vulnerable malloc

---
+++ vulnerable_malloc.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(){
+char *name;
+name = (char *)malloc(5);
+printf("enter name:");
+fgets(name,100,stdin);
+printf("Hello %s\n",name);
+free(name);
+return 0;
+}
+
